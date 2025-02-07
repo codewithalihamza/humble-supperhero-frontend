26ec93146f58355f01562a2e0c3987889e167e5b
codewithalihamza <512syedalihamza@gmail.com>
2025-02-08 00:01:48 +0500

    Simple Form & List Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [name, setName] = useState('');
+  const [superpower, setSuperpower] = useState('');
+  const [humilityScore, setHumilityScore] = useState(5);
+  const [superheroes, setSuperheroes] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/superheroes').then((res) => setSuperheroes(res.data));
+  }, []);
+
+  const addSuperhero = async () => {
+    await axios.post('http://localhost:5000/superheroes', { name, superpower, humilityScore });
+    const res = await axios.get('http://localhost:5000/superheroes');
+    setSuperheroes(res.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Humble Superheroes</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+      <input value={superpower} onChange={(e) => setSuperpower(e.target.value)} placeholder="Superpower" />
+      <input type="number" value={humilityScore} min="1" max="10" onChange={(e) => setHumilityScore(Number(e.target.value))} />
+      <button onClick={addSuperhero}>Add Superhero</button>
+
+      <h2>Leaderboard</h2>
+      <ul>
+        {superheroes.map((hero) => (
+          <li key={hero.name}>{hero.name} ({hero.humilityScore})</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
